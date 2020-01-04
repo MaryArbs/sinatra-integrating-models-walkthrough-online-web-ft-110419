7fb3449afb1377788aa7cf25753c69a959ba7860
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2020-01-04 22:51:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
+    @user_text = params[:user_text]
     text_from_user = params[:user_text]
     @analyzed_text = TextAnalyzer.new(text_from_user)
     erb :results
